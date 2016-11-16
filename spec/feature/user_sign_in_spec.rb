a8678929054722a9104548bd0c79e020b40f5a33
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-11-16 23:39:57 +0000

    Add sign in feature test

---
+++ spec/feature/user_sign_in_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "User sign in", type: :feature do
+  subject { visit new_user_session_url }
+
+  context "when user is registered" do
+    before do
+      user = create(:user)
+      subject
+      fill_in "user_email", with: user.email
+      fill_in "user_password", with: user.password
+      click_on "sign_in"
+    end
+
+    it "redirects to root path" do
+      expect(page).to have_current_path(root_path)
+    end
+  end
+
+  context "when user is not registered" do
+    before { subject }
+
+    it "not redirects to another page" do
+      expect(page).to have_current_path(new_user_session_path)
+    end
+  end
+end
