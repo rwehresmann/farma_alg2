b1f6dfa45ea1d452872e55f95a43d8700bc27918
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-11-16 23:40:52 +0000

    Create welcome index view and update header according user session

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,6 @@ class WelcomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if user_signed_in?
-      redirect_to dashboard_home_path
-    end
+    
   end
 end
