6b9ef752bfeb20f5acfd823b2c23b0f293771d07
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-11-20 22:13:57 +0000

    Add name and prof to permited attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :prof) }
+    # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
+  end
 end
