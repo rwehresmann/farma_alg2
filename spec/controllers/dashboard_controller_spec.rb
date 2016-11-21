5588062b0f164443d39a7c8120c51f887dd4b82c
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-11-21 14:25:55 +0000

    Add basic controller tests

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe DashboardController, type: :controller do
+describe DashboardController, type: :controller do
+  describe "Get #home" do
+    before { get :home }
 
+    it "renders home view" do
+      expect(response).to render_template(:home)
+    end
+  end
 end
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe StaticPagesController, type: :controller do
+describe StaticPagesController, type: :controller do
+  describe "Get #home" do
+    before { get :home }
 
+    it "renders home view" do
+      expect(response).to render_template(:home)
+    end
+  end
 end
