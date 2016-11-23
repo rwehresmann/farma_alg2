ca5fbaddf7d260f67f256c93ae0e33454312c544
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-11-23 23:26:45 +0000

    Add password_confirmation in permited parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :prof) }
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :prof) }
       # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
     end
 
