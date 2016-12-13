6773b032e6d182b2b6b92944035b3de52e0c7dbb
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-12-13 22:42:14 +0000

    Add model process_queue

---
+++ app/models/process_queue.rb
@@ -0,0 +1,66 @@
+class ProcessQueue
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :type
+  field :priority, type: Integer, default: 9
+  field :params, type: Array, default: []
+
+  before_create :verify_repeat
+
+  def verify_repeat
+    unless ProcessQueue.where(type: self.type, params: self.params, priority: self.priority).count == 0
+      return false
+    end
+    return true
+  end
+
+  def do_process
+  	p = self
+
+    #self.delete
+
+  	case p.type
+
+    when "apply_primary_tags"
+       puts "Process: apply_primary_tags(#{p.params[0]}) started ..."
+       Answer.find(p.params[0]).apply_primary_tags if Answer.where(id: p.params[0]).any?
+       puts "Process: apply_primary_tags(#{p.params[0]}) ended ..."
+
+   	when "make_inner_connections"
+  		puts "Process: make_inner_connections(#{p.params[0]}) started ..."
+  		Answer.find(p.params[0]).make_inner_connections if Answer.where(id: p.params[0]).any?
+
+  		puts "Process: make_inner_connections(#{p.params[0]}) ended ..."
+
+  	when "make_outer_connections"
+  		puts "Process: make_outer_connections(#{p.params[0]}) started ..."
+  		Answer.find(p.params[0]).make_outer_connections if Answer.where(id: p.params[0]).any?
+
+  		puts "Process: make_outer_connections(#{p.params[0]}) ended ..."
+
+  	when "propagate_properties"
+  		puts "Process: propagate_properties(#{p.params[0]}) started ..."
+  		Answer.find(p.params[0]).propagate_properties if Answer.where(id: p.params[0]).any?
+
+  		puts "Process: propagate_properties(#{p.params[0]}) ended ..."
+  	else
+  		puts "Process type unknown."
+  		return false
+  	end
+
+    self.delete
+
+  	return true
+  end
+
+  def self.start
+  	while true
+  		while ProcessQueue.count > 0
+  			p = ProcessQueue.asc(:priority,:created_at).first
+  			p.do_process
+  		end
+  		sleep 1
+  	end
+  end
+end
---
+++ spec/factories/process_queues.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :process_queue do
+    
+  end
+end
